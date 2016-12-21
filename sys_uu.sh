9cdae099afa662314faf477a91a161aa6ed8604c
Hojeong Lee <cafebear@nate.com>
2016-12-21 13:36:05 +0900

    modified sys_uu.sh

--- sys_uu.sh
+++ sys_uu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "sudo apt-get update ; sudo apt-get upgrade -y ; sudo apt-get dist-upgrade -y ; sudo rpi-update"
+echo "sudo apt-get update ; sudo apt-get upgrade -y ; sudo apt-get dist-upgrade -y ; sudo rpi-update; sudo apt-get autoclean"
 
 
-time sudo apt-get update ; time sudo apt-get upgrade -y ; time sudo apt-get dist-upgrade -y ; time sudo rpi-update
+time sudo apt-get update ; time sudo apt-get upgrade -y ; time sudo apt-get dist-upgrade -y ; time sudo rpi-update; sudo apt-get autoclean
